546ba9df4c59a46a6a3d02adfd0c62609442ca21
kebernet <kebernet@d620f1b1-8330-0410-a80d-d7860aac830e>
2007-07-23 02:18:29 +0000

    Adding 0.1.3

---
+++ tags/release-0.1.3/gwittir-core/src/main/java/com/totsp/gwittir/client/flow/FlowContext.java
@@ -0,0 +1,84 @@
+/*
+ * FlowContext.java
+ *
+ * Created on May 9, 2007, 6:29 PM
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package com.totsp.gwittir.client.flow;
+
+import com.totsp.gwittir.client.action.Action;
+import com.totsp.gwittir.client.ui.BoundWidget;
+
+import java.util.HashMap;
+
+
+/**
+ *
+ * @author cooper
+ */
+public class FlowContext {
+    private final HashMap actions = new HashMap();
+    private final HashMap destinations = new HashMap();
+
+    /** Creates a new instance of FlowContext */
+    public FlowContext() {
+        super();
+    }
+
+    public FlowContext add(String name, BoundWidget widget) {
+        destinations.put(name, widget);
+
+        return this;
+    }
+
+    public FlowContext add(String name, BoundWidgetProvider provider) {
+        destinations.put(name, provider);
+
+        return this;
+    }
+
+    public FlowContext add(String name, BoundWidget widget, Action action) {
+        add(name, widget);
+        actions.put(name, action);
+
+        return this;
+    }
+
+    public FlowContext add(String name, BoundWidgetProvider provider,
+        Action action) {
+        add(name, provider);
+        actions.put(name, action);
+
+        return this;
+    }
+
+    public BoundWidget get(String name) {
+        Object value = destinations.get(name);
+        BoundWidget ret;
+
+        if(value instanceof BoundWidgetProvider) {
+            ret = ((BoundWidgetProvider) value).get();
+        } else {
+            ret = (BoundWidget) destinations.get(name);
+        }
+
+        if((ret != null) && (actions.get(name) != null)) {
+            ret.setAction((Action) actions.get(name));
+        }
+
+        return ret;
+    }
+}
---
+++ tags/release-0.1.3/gwittir-core/src/main/java/com/totsp/gwittir/example/client/ExampleEntryPoint.java
@@ -0,0 +1,132 @@
+/*
+ * ExampleEntryPoint.java
+ *
+ * Created on April 12, 2007, 4:47 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+package com.totsp.gwittir.example.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.totsp.gwittir.client.action.Action;
+
+import com.totsp.gwittir.client.validator.PopupValidationFeedback;
+import com.totsp.gwittir.client.validator.ValidationFeedback;
+import com.totsp.gwittir.client.beans.BeanDescriptor;
+import com.totsp.gwittir.client.beans.Binding;
+import com.totsp.gwittir.client.beans.Introspector;
+import com.totsp.gwittir.client.beans.Property;
+import com.totsp.gwittir.client.ui.BoundWidget;
+import com.totsp.gwittir.client.ui.Button;
+import com.totsp.gwittir.client.ui.SoftButton;
+import com.totsp.gwittir.client.ui.TextBox;
+import com.totsp.gwittir.client.validator.CompositeValidator;
+import com.totsp.gwittir.client.validator.IntegerRangeValidator;
+import com.totsp.gwittir.client.validator.IntegerValidator;
+import com.totsp.gwittir.client.validator.NotNullValidator;
+
+
+/**
+ *
+ * @author cooper
+ */
+public class ExampleEntryPoint implements EntryPoint {
+    /** Creates a new instance of ExampleEntryPoint */
+    public ExampleEntryPoint() {
+    }
+    
+    public void onModuleLoad() {
+        Foo model = new Foo();
+        model.setIntProperty(3);
+        model.setStringProperty("Foo bar baz");
+        
+        FooEdit edit = new FooEdit(model);
+        edit.setModel(model);
+        RootPanel.get().add(edit);
+        
+        Introspector is = (Introspector) GWT.create(Introspector.class);
+        BeanDescriptor bd = is.getDescriptor(model);
+        Object[] newValue = { new Integer(-255) };
+        
+        SoftButton button = new SoftButton("Validate");
+        
+        button.setAction( new Action(){
+            public void execute(BoundWidget model) {
+                GWT.log("Action Fired", null);
+                //Window.alert( ""+b.isValid() );
+            }
+            
+        });
+        button.setSize( "200px", "200px");
+        button.addClickListener( new ClickListener(){
+            public void onClick(Widget sender) {
+                GWT.log( "CLICKED", null);
+            }
+            
+        });
+        
+        RootPanel.get().add( button );
+        
+        try {
+            bd.getProperty("intProperty").getMutatorMethod()
+            .invoke(model, newValue);
+            GWT.log((String) bd.getProperty("stringProperty").getAccessMethod()
+            .invoke(model, null), null);
+        } catch(Exception e) {
+            GWT.log("AAAH!  ", e);
+        }
+        
+        Property[] props = bd.getProperties();
+        
+        for(int i = 0; i < props.length; i++) {
+            GWT.log("" + props[i].getName(), null);
+        }
+        
+        TextBox box = new TextBox(true);
+        TextBox intBox = new TextBox(true);
+        
+        CompositeValidator cv = new CompositeValidator()
+        .add(new NotNullValidator())
+        .add(new IntegerValidator())
+        .add(new IntegerRangeValidator(1,5));
+        ValidationFeedback pvf = new PopupValidationFeedback(PopupValidationFeedback.BOTTOM)
+        .addMessage(NotNullValidator.class, "You must provide a value")
+        .addMessage(IntegerValidator.class, "Enter an integer")
+        .addMessage(IntegerRangeValidator.class, "Value must be 1..5");
+        
+        final Binding b = new Binding(box, "value", model, "stringProperty");
+        Binding b2 = new Binding(intBox, "value", cv, pvf, model,
+                "intProperty", null, null);
+        
+        b.getChildren().add( b2 );
+        b.bind();
+        b.setLeft();
+
+        
+        button.setAction( new Action(){
+            public void execute(BoundWidget model) {
+                GWT.log("Action Fired", null);
+                Window.alert( ""+b.isValid() );
+            }
+            
+        });
+        button.setSize( "200px", "200px");
+        button.addClickListener( new ClickListener(){
+            public void onClick(Widget sender) {
+                GWT.log( "CLICKED", null);
+            }
+            
+        });
+        
+        RootPanel.get().add(box);
+        RootPanel.get().add(intBox);
+    }
+}
